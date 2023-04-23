9a2814f1640ed8641d298356051ab942647f8cf0
Digonil <diego.niles@hotmail.com>
2023-04-23 11:52:51 -0300

    Retirado a classe CriadorJson pois não foi necessária a sua utilização

--- src/br/com/niles/CriadorJson.java
+++
@@ -1,8 +0,0 @@
-package br.com.niles;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-public class CriadorJson {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-}
